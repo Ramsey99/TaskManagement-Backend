3d0cc7fce90f4fa47dbb984926a5cabfa0220faf
Ramsey99 <adhikarianuradha336@gmail.com>
2025-10-26 16:02:36 +0530

    baseurl updated

--- baseUrl.js
+++ baseUrl.js
@@ -1,3 +1,3 @@
-const base_url = "http://localhost:5000/uploads";
+const base_url = "https://taskmanagement-backend-55hv.onrender.com/uploads";
 module.exports = base_url;
 console.log("Base url is working");
